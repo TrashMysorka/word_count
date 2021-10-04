2c1acf964118ccd46dab72b0bc4f512a31b93f15
v.babushkin <ghostminerx@gmail.com>
2021-10-04 16:44:09 +0700

    little fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 #include <list>
 #include <iterator>
 #include <map>
-#include <cstring>
 using namespace std;
 map<string, int> words;
 bool amount_comp(const std::string& first, const std::string& second){
@@ -13,8 +11,8 @@ bool amount_comp(const std::string& first, const std::string& second){
 bool size_comp(const std::string& first, const std::string& second){
     return first < second;
 }
-void get_word(string row, list<string>& rows, map<string, int>& words, int& count){
-    string word = "";
+void get_word(string row, list<string>& rows, int& count){
+    string word;
 
     for(int i = 0; i < row.size(); i++){
         if (isalnum(row[i])){
@@ -33,7 +31,18 @@ void get_word(string row, list<string>& rows, map<string, int>& words, int& coun
         }
     }
 }
+void sort_list(list<string>& rows){
+    rows.sort(size_comp);
+    rows.unique();
+    rows.sort(amount_comp);
+}
+void print_words( list<string>& rows, int& count,ofstream& output){
+    for (auto const& i : rows){
+        output << i <<"," << words[i] <<"," << (float(words[i]) / float(count))*100 << "%" <<"\n" ;
+    }
+}
 int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
     ifstream input(argv[1]);
     ofstream output(argv[2]);
     string str;
@@ -41,13 +50,10 @@ int main(int argc, char** argv) {
     int count = 0;
     while(!input.eof()){
         getline(input, str);
-        get_word(str, rows, words, count);
+        get_word(str, rows, count);
     }
-    rows.sort(size_comp);
-    rows.unique();
-    rows.sort(amount_comp);
-    for (auto const& i : rows){
-        output << i <<"," << words[i] <<"," << (float(words[i]) / float(count))*100 << "%" <<"\n" ;
-    }
+    sort_list(rows);
+
+    print_words(rows, count, output);
     return 0;
 }
